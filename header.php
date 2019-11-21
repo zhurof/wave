a8531d1d50cbff406efba5d6cde769597a755761
zhurof <41473707+zhurof@users.noreply.github.com>
2019-11-21 15:42:39 +0300

    Initial commit

---
+++ header.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<title>Верстка</title>
+	<meta name="viewport" content="width=device-width, shrink-to-fit=no" />	
+	<link rel="stylesheet" href="css/fonts.css" />	
+	<link rel="stylesheet" href="css/font-awesome.css" />
+	<link rel="stylesheet" href="libs/slick/slick.css" />
+	<link rel="stylesheet" href="css/style.css" />
+</head>
+<body>
+	<header class="header">
+		
+	</header>
+<!-- Переход на страницу со ссылками на современные браузеры для ie9 и ниже -->
+<!--[if lt IE 10]><script>location.href='/old-ie.html';</script><![endif]-->
---
+++ index.php
@@ -0,0 +1,6 @@
+﻿<?php	include "header.php";?>		
+	<main>
+		<div class="foo-widget" data-foo-id="1"></div>
+		<script src="http://widget.belturizm.by:44405/3rd/foo/platform.js?partyId=ivrgwymaaut"></script>		
+	</main>
+<?php	include "footer.php";?>	
